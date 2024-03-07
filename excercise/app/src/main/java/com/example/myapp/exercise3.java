32af71f2ca0074d992f2c8f921a9cc6a6b29485f
Nguyen-Van-Truong-Khoa <107803167+Nguyen-Van-Truong-Khoa@users.noreply.github.com>
2024-03-08 00:33:07 +0700

    ex1-ex4

---
+++ excercise/app/src/main/java/com/example/myapp/exercise3.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exercise3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise3);
+    }
+}
